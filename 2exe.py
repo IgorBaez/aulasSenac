dd1eab0da559fbb86b549c59609f272868ee0167
igorBaez <igorbaez123@gmail.com>
2024-09-26 14:04:22 -0400

    aula

---
+++ 2exe.py
@@ -0,0 +1,20 @@
+
+a="Ederson"
+print(len(a))    #len serve para contar a quantidade de caracter dentro da variavel em str
+b="bazzz"
+c=len(b)
+print(c)
+
+print(b.capitalize())   #.capitalize serve para trasnforma a primeira letra da primeira palavra em maiuscula 
+
+
+
+
+
+
+
+
+
+
+
+
