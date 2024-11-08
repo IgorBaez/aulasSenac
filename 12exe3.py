190f21253a784533a7e78038f716e0ab6d6b0b6c
IgorGBaez <igorbaez123@gmail.com>
2024-11-07 20:41:58 -0400

    Create 12exe3.py

---
+++ 12exe3.py
@@ -0,0 +1,16 @@
+def result(a,b):
+    b+=b/100
+    soma=a+b
+    return soma
+a=int(input("taxa "))
+b=int(input("custo "))
+
+print("Valor total e de ",result(a,b),"o valor de custo e de ",b,"valor da taxa e de ",a)
+
+
+
+
+
+
+
+
