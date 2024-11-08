423d7f09b67897d81a5a3fe4b9a2e1733b8876e4
IgorGBaez <igorbaez123@gmail.com>
2024-11-07 20:42:33 -0400

    Create 2exept2.py

---
+++ 2exept2.py
@@ -0,0 +1,31 @@
+
+af="Igor Gonçalo Baez"
+print(af.count("z"))     #a count retorna quantas vezes o item selecionado
+
+
+x="teste Baez"
+print("Baez"in x)   #o in verifica se uma substring e parte de uma outra string
+
+
+print(af.find("Baez"))  #o .find retorna onde a substring comeca na string
+
+
+ #-1-2-3-4-5-6-7-8-9-10 da direita pra esquerda 
+s="O l a , m u n d o !"
+#  0 1 2 3 4 5 6 7 8 9 da esquerda pra direita
+s1="Ola,mundo!"
+print(s1[1:6])     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
