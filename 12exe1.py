57eae6614588933b3c5e57d128f2f64b98f46243
IgorGBaez <igorbaez123@gmail.com>
2024-11-07 20:41:52 -0400

    Create 12exe1.py

---
+++ 12exe1.py
@@ -0,0 +1,16 @@
+def calculo(x,y,z):
+    cal=(x+y)*z
+    return cal
+
+x=int(input("primeiro numero"))
+y=int(input("segundo numero"))
+z=int(input("terceiro numero"))
+cal=calculo(x,y,z)
+print(cal)
+
+
+
+
+
+
+
